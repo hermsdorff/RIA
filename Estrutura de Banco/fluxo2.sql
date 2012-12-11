1445a86038db1aea5445342b9329234eac899f97
hermsdorff <kleber.hermsdorff@gmail.com>
2012-12-10 22:09:22 -0200

    adaptado para o banco fornecido pelo professor

--- Estrutura de Banco/fluxo2.sql
+++ Estrutura de Banco/fluxo2.sql
@@ -1,30 +1,30 @@
 --
--- Banco de Dados: `fluxo2`
+-- Banco de Dados: `fluxo`
 --
 
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL,
-  `senha` varchar(25) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=23;
-
-CREATE TABLE conta (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  conta_id INT(11) NULL,
-  nome VARCHAR(100) NOT NULL,
-  flag_tipo SMALLINT(1) UNSIGNED NOT NULL,
-  PRIMARY KEY(id),
-  INDEX fk_conta_conta(conta_id)
+CREATE TABLE teContas (
+  idContas INT(11) NOT NULL AUTO_INCREMENT,
+  teContas_idContas INT(11) NULL,
+  NmConta VARCHAR(100) NOT NULL,
+  FgTipo SMALLINT(1) UNSIGNED NOT NULL,
+  PRIMARY KEY(idContas),
+  INDEX teContas_FKIndex1(teContas_idContas)
 );
 
-CREATE TABLE fluxo (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  conta_id INT(11) NOT NULL,
-  descricao VARCHAR(255) NOT NULL,
-  dt_fluxo DATE NOT NULL,
-  valor FLOAT(8,2) NOT NULL,
-  PRIMARY KEY(id),
-  INDEX fk_fluxo_conta(conta_id)
+CREATE TABLE teFluxo (
+  idFluxo INT(11) NOT NULL AUTO_INCREMENT,
+  teContas_idContas INT(11) NOT NULL,
+  DsDescricao VARCHAR(255) NOT NULL,
+  DtFluxo DATE NOT NULL,
+  NuValor FLOAT(8,2) NOT NULL,
+  PRIMARY KEY(idFluxo),
+  INDEX teFluxo_FKIndex1(teContas_idContas)
+);
+
+CREATE TABLE teUsuario (
+  idUsuario INT(11) NOT NULL AUTO_INCREMENT,
+  NmUsuario VARCHAR(255) NOT NULL,
+  DsLogin VARCHAR(25) NOT NULL,
+  DsSenha VARCHAR(100) NOT NULL,
+  PRIMARY KEY(idUsuario)
 );
